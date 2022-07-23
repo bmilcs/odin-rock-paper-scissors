080a4fe273d3dbb959d5bbca89431b53aed008c4
bmilcs <bmilcs@yahoo.com>
2022-07-23 18:27:27 -0400

    Add plan / pseudocode for playRound to javascript.md

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // Test to see script.js is being loaded:
 console.log("Welcome to Bryan Miller's Rock Paper Scissors Project");
 
-
-
 // Function: Randomly return Rock, Paper or Scissors
 function getComputerChoice() {
   // Create an array with values rock, paper, Scissors
@@ -21,24 +19,36 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
-
-
 // Function that plays a single round of Rock Paper Scissors
 function playRound(playerSelection, computerSelection) {
   // Convert playerSelection to Title Case
   playerSelection = toTitleCase(playerSelection);
 
   // If player & computer select the same thing, it's a tie
+  // If player = rock, computer = scissors, player wins
+  // If player = rock & computer = paper, computer wins
+  // If player = scissors & computer = rock, computer wins
+  // If player = scissors & computer = paper, player wins
+  // If player = paper & computer = rock, player wins
+  // If player = paper & computer = scissors, player wins
+
   if (playerSelection === computerSelection) {
     return `It's a tie! You both chose ${playerSelection}!`;
-  } else if 
 
-  // If playerSelection is Rock, it beats scissors
-  if (playerSelection === "Rock" && computerSelection === "Scissors") {
-    console.log("Bingo");
+    // If player = rock
+  } else if (playerSelection === "Rock") {
+    switch (computerSelection) {
+      case "Scissors":
+        console.log("You Win! Rock beats Scissors.");
+        break;
+      case "Paper":
+        console.log("You Lose! Paper beats Rock.");
+        break;
+    }
   }
 
-  return computerSelection + " " + playerSelection;
+  // Debugging: Return choices
+  return "Comp: " + computerSelection + " | You: " + playerSelection;
 }
 
 // Function: Convert string to title case (from exercises)
@@ -50,8 +60,6 @@ function toTitleCase(string) {
   // Replace first character of string with capitalized version
   return titleCase.replace(/^./, firstLetter.toUpperCase());
 }
-
-
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
