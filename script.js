99690ada02211f02ac703a1ae968d5d977c9c7f1
bmilcs <bmilcs@yahoo.com>
2022-07-23 18:18:12 -0400

    Add playRound & toTitleCase functions to script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Test to see script.js is being loaded:
 console.log("Welcome to Bryan Miller's Rock Paper Scissors Project");
 
+
+
 // Function: Randomly return Rock, Paper or Scissors
 function getComputerChoice() {
   // Create an array with values rock, paper, Scissors
@@ -18,3 +20,40 @@ function getRandomInt(min, max) {
   // Generate a random number between two values:
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
+
+
+
+// Function that plays a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+  // Convert playerSelection to Title Case
+  playerSelection = toTitleCase(playerSelection);
+
+  // If player & computer select the same thing, it's a tie
+  if (playerSelection === computerSelection) {
+    return `It's a tie! You both chose ${playerSelection}!`;
+  } else if 
+
+  // If playerSelection is Rock, it beats scissors
+  if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    console.log("Bingo");
+  }
+
+  return computerSelection + " " + playerSelection;
+}
+
+// Function: Convert string to title case (from exercises)
+function toTitleCase(string) {
+  // Convert string to lower case
+  let titleCase = string.toLowerCase();
+  // Get first letter of string
+  let firstLetter = titleCase.charAt();
+  // Replace first character of string with capitalized version
+  return titleCase.replace(/^./, firstLetter.toUpperCase());
+}
+
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, getComputerChoice()));
