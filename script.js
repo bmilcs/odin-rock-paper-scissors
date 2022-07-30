487ee51b81bdc73383810523715f92860e71547d
bmilcs <bmilcs@yahoo.com>
2022-07-30 18:57:31 -0400

    Add color & timeouts to rounds/game

--- script.js
+++ script.js
@@ -8,44 +8,71 @@ let gameResult = document.querySelector(".gameResult");
 let playerScore = 0;
 let computerScore = 0;
 let gameCount = 0;
+let roundTimeOut = 2000;
 
+// Event: Begin round when icon is clicked
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     console.log(playRound(button.id, getComputerChoice()));
+    button.style.backgroundColor = "#11cc80";
   });
 });
 
 // Function: Play a single round of rock paper scissors
-function playRound(playerSelection, computerSelection) {
+let playRound = function (playerSelection, computerSelection) {
+  let computerIcon = document.getElementById(computerSelection);
+  computerIcon.style.backgroundColor = "#db4437";
   if (playerSelection == computerSelection) {
-    gameResult.textContent = `It's a Tie! Try again!`;
+    // gameResult.textContent = `It's a Tie! Try again!`;
+    playRound(playerSelection, getComputerChoice());
+    return;
   } else if (
     (playerSelection === "rock" && computerSelection == "scissors") ||
     (playerSelection === "scissors" && computerSelection == "paper") ||
     (playerSelection === "paper" && computerSelection == "rock")
   ) {
+    gameResult.style.color = "#11cc80";
     gameResult.textContent = `You Win!\t${playerSelection} beats ${computerSelection}!`;
     playerScoreBoard.textContent = ++playerScore;
   } else {
     gameResult.textContent = `You Lose!\t${computerSelection} beats ${playerSelection}`;
+    gameResult.style.color = "#db4437";
     computerScoreBoard.textContent = ++computerScore;
   }
 
   // Score Check
   if (playerScore + computerScore === 5) {
     tallyResults();
+  } else {
+    window.setTimeout(() => {
+      gameResult.textContent = `Round ${playerScore + computerScore + 1}`;
+      roundReset();
+    }, roundTimeOut);
   }
+};
+
+// After each round, reset icons & gameResult
+function roundReset() {
+  buttons.forEach((button) => (button.style.backgroundColor = "white"));
+  gameResult.style.color = "black";
 }
 
 // Function: Tally results of 5 games
 function tallyResults() {
-  if (playerScore > computerScore) {
-    gameResult.textContent = "YOU WIN!!!";
-  } else {
-    gameResult.textContent = "LOOOOOSER!";
-  }
-  computerScoreBoard.textContent = computerScore = 0;
-  playerScoreBoard.textContent = playerScore = 0;
+  window.setTimeout(() => {
+    if (playerScore > computerScore) {
+      gameResult.textContent = "Congratulations! You are the winner!";
+    } else {
+      gameResult.textContent =
+        "Another one bites the dust! The computer is the winner!";
+    }
+  }, roundTimeOut);
+  window.setTimeout(() => {
+    computerScoreBoard.textContent = computerScore = 0;
+    playerScoreBoard.textContent = playerScore = 0;
+    gameResult.textContent = "Round 1";
+    roundReset();
+  }, 5000);
 }
 
 // Function: Randomly return rock, paper or scissors for Computer's Selection
